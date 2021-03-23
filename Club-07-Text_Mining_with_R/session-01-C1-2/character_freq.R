06e9eeb6552fc3e0b4bc227f4ef51e4a80bd5839
John Peach <githubspam@jppeach.com>
2021-03-22 19:24:21 -0700

    updated frequency solution

--- Club-07-Text_Mining_with_R/session-01-C1-2/character_freq.R
+++ Club-07-Text_Mining_with_R/session-01-C1-2/character_freq.R
@@ -1,16 +1,11 @@
 library(tidytext)
-library(gutenbergr)
+library(janeaustenr)
 
-
-df <- gutenberg_download(1661)
-df <- austen_books()
-df %>%
+austen_books() %>%
   select(text) %>%
-  dplyr::filter(length(text) > 0) %>%
   unnest_tokens(characters, text, token = 'characters') %>%
   count(characters) %>%
   mutate(frequency = n / sum(n)) %>%
-  arrange(desc(n)) %>%
   ggplot(aes(x = reorder(characters, frequency), y = frequency)) +
     geom_col() +
     coord_flip() +
