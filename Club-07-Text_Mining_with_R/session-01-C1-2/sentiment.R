4ba7349667fe21331bd8fcf567796f9b211fb6c7
John Peach <githubspam@jppeach.com>
2021-03-22 20:10:56 -0700

    clean up sentiment

--- Club-07-Text_Mining_with_R/session-01-C1-2/sentiment.R
+++ Club-07-Text_Mining_with_R/session-01-C1-2/sentiment.R
@@ -2,18 +2,15 @@ library(tidytext)
 library(janeaustenr)
 
 austen_books() %>%
-  group_by(book) %>%
+  dplyr::filter(book == 'Sense & Sensibility') %>%
   mutate(linenumber = row_number(),
          chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
                                                  ignore_case=TRUE)))) %>%
-  ungroup() %>%
   unnest_tokens(word, text) %>%
-  dplyr::filter(book == 'Sense & Sensibility') %>%
   inner_join(get_sentiments('afinn')) %>%
   group_by(chapter) %>%
   select(chapter, value) %>%
   dplyr::summarise(sentiment = mean(value)) %>%
-  arrange(desc(sentiment)) %>%
   ggplot(aes(x = chapter, y = sentiment)) +
     geom_line() +
     theme_minimal() +
