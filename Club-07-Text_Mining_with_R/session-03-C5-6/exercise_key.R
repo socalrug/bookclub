55723d779c09064fa358a71b2455723960b5cecb
Lily Cheng <youc3@uci.edu>
2021-04-05 22:25:45 -0400

    chap5-6

---
+++ Club-07-Text_Mining_with_R/session-03-C5-6/exercise_key.R
@@ -0,0 +1,60 @@
+library(tm)
+
+data("AssociatedPress", package = "topicmodels")
+AssociatedPress
+
+terms <- Terms(AssociatedPress)
+head(terms)
+
+library(dplyr)
+library(tidytext)
+
+ap_td <- tidy(AssociatedPress)
+ap_td
+
+ap_sentiments <- ap_td %>%
+  inner_join(get_sentiments("afinn"), by = c(term = "word"))
+
+ap_sentiments
+
+library(ggplot2)
+
+ap_sentiments %>%
+  count(value, term, wt = count) %>%
+  ungroup() %>%
+  filter(n >= 200) %>%
+  mutate(n = ifelse(value < 0, -n, n)) %>%
+  mutate(term = reorder(term, n)) %>%
+  ggplot(aes(n, term, fill = value)) +
+  geom_col() +
+  labs(x = "Contribution to sentiment", y = NULL)
+
+# --------------------------------
+library(topicmodels)
+
+data("AssociatedPress")
+
+ap_lda <- LDA(AssociatedPress, k = 3, control = list(seed = 1234))
+
+ap_lda
+
+library(tidytext)
+
+ap_topics <- tidy(ap_lda, matrix = "beta")
+ap_topics
+
+library(ggplot2)
+library(dplyr)
+
+ap_top_terms <- ap_topics %>%
+  group_by(topic) %>%
+  top_n(15, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+
+ap_top_terms %>%
+  mutate(term = reorder_within(term, beta, topic)) %>%
+  ggplot(aes(beta, term, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  scale_y_reordered()
